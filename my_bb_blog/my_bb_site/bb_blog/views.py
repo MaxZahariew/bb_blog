afeedd249b2836e8d5f77861e9c88fd1fa1170bc
Maxim <maxim.zahariew@yandex.ru>
2023-12-30 14:32:54 +0700

    Добавлена постраничеъная разбивка.

--- my_bb_blog/my_bb_site/bb_blog/views.py
+++ my_bb_blog/my_bb_site/bb_blog/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, \
+    PageNotAnInteger
 
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.published.all()
-    paginator = Paginator(posts, 3)
+    posts_list = Post.published.all()
+    # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(posts_list, 1)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # Если page_number находится вне диапазона, то
+        # выдать последнюю страницу результатов
+        posts = paginator.page(number=paginator.num_pages)
+    except PageNotAnInteger:
+        # Если page_number не целое число, то
+        # выдать первую страницу
+        posts = paginator.page(number=1)
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
